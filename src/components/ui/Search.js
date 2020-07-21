5b10011a3ad07cd40c0444be2d0c49c38df1ca32
Maciej Perzankowski <perzankowski.m2001@gmail.com>
2020-07-21 17:06:11 +0200

    adding search input and spinner

--- src/components/ui/Search.js
+++ src/components/ui/Search.js
@@ -1,10 +1,24 @@
 import React, {useState} from "react";
 
-const Search = () => {
+const Search = ({getQuery}) => {
+  const [text, setText] = useState('')
+
+  const onChange = (q) =>{
+    setText(q)
+    getQuery(q)
+  }
+
   return (
     <section className="search">
       <form>
-        <input type="text" className="form-control" placeholder="Search character" autoFocus />
+        <input
+          type="text"
+          className="form-control"
+          placeholder="Search character"
+          autoFocus
+          onChange={(e) => onChange(e.target.value)}
+          value={text}
+        />
       </form>
     </section>
   )
